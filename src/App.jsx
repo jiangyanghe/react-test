e2696737dd1fadb9cf92dbbc1ba68d2181551fa2
jiangyanghe <1299657921@qq.com>
2022-07-04 22:26:40 +0800

    feat(): demo shouldComponentUpdate

--- src/App.jsx
+++ src/App.jsx
@@ -19,7 +19,7 @@ function App() {
         </button> */}
 
        {/* /* ************* ClassComponents *********** */}
-        {/* <ClassComponents /> */}
+        <ClassComponents />
     </>
   )
 }
--- src/classComponents.jsx
+++ src/classComponents.jsx
@@ -2,33 +2,54 @@ import { Component, PureComponent } from 'react';
 
 class ClassComponents extends Component {
   state = {
-    name: '张三'
+    name: '张三',
+    other: {
+      age: 18,
+      job: 'waiter'
+    }
   }
 
   componentDidMount() {
-    setInterval(() => {
-      this.updateName();
-    }, 1000)
+    // setInterval(() => {
+    //   this.updateName();
+    // }, 2000)
   }
 
   updateName = () => {
     this.setState({
-      name: '张三'
+      name: '张三',
+    })
+  }
+
+  changeJob = () => {
+    const { other: { job } } = this.state;
+    this.setState({
+      ...this.state,
+      job: 'chef'
     })
   }
 
   render() {
     return (
-<>
-      <ReguarComponentDemo {...this.state} />
-      <PureComponentDemo {...this.state} />
-    </>
+      <>
+        <ReguarComponentDemo {...this.state} />
+        <PureComponentDemo {...this.state} />
+        <button onClick={this.changeJob}>change job</button>
+      </>
     )
-    
+
   }
 }
 
 class ReguarComponentDemo extends Component {
+  shouldComponentUpdate(nextProps, nextState){
+    // console.log('PureComponent==', nextProps, nextState);
+    if (nextProps.name !== this.props.name) {
+      return true
+    }
+    return false
+  }
+
   render() {
     console.log('Component==');
 
@@ -37,6 +58,7 @@ class ReguarComponentDemo extends Component {
 }
 
 class PureComponentDemo extends PureComponent {
+  
   render() {
     console.log('PureComponent==');
 
